97e07c1eedf8ffba88277a7c8169ee8240de4dd6
Zack <zack@ZackDator.local>
2024-08-23 20:19:20 +0200

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import router from "./router";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("GET request received");
+  res.status(200).send(res.json({ message: "Hello, world!" }));
+});
+
+app.use("/api", router);
+export default app;
